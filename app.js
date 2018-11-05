94b3823d48bc8a78daf02acf5c1357e815f118bb
Jonah Engelund <jonahengelund@gmail.com>
2018-11-05 15:30:32 -0700

    working game

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
 // variables!!
-let health = 100;
-let hits = 0;
-let name = "StrikeFreedom"
+let target = {
+    name: "Strike Freedom",
+    health: 100,
+    hits: 0,
+    items: [],
+}
 
+
+let items = {
+    fire: { name: "Fire", modifier: 5, description: "it burns!" },
+    super: { name: "Super", modifier: 10, description: "Shwang!" },
+    special: { name: "Special", modifier: 15, description: "Bang!" },
+}
 
 // Functions!!!
-function slap() {
-    health -= 1
-    hit()
-}
 function punch() {
-    health -= 5
+    target.health -= 1 + addMods();
     hit()
 }
-function kick() {
-    health -= 10
+function gun() {
+    target.health -= 5 + addMods();
+    hit()
+}
+function sword() {
+    target.health -= 10 + addMods();
     hit()
 }
 function hit() {
-    hits += 1
+    target.hits += 1
     update()
 }
+function giveFire() {
+    target.items.push(items.fire)
+
+}
+function giveSuper() {
+    target.items.push(items.super)
+
+}
+function giveSpecial() {
+    target.items.push(items.special)
+
+}
+function reset() {
+    document.location.href = ""
+}
+function addMods() {
+    let total = 0;
+    let items = target.items;
+    for (let index = 0; index < items.length; index++) {
+        let item = items[index];
+        total += item.modifier;
+    }
+    return total;
+}
+
 function update() {
-    document.getElementById("health").innerText = health
-    document.getElementById("name").innerText = name
-    document.getElementById("hits").innerText = hits
+    document.getElementById("health").innerText = target.health
+    document.getElementById("name").innerText = target.name
+    document.getElementById("hits").innerText = target.hits
 }
